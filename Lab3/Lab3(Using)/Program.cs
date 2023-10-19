356d749de94f5a8f8ea3b01510aa2dccd4f96314
Yehorkk <142511207+Yehorkk@users.noreply.github.com>
2023-10-19 23:35:49 +0300

    add NuGet package to Lab3

---
+++ Lab3/Lab3(Using)/Program.cs
@@ -0,0 +1,18 @@
+﻿using CL3;
+
+namespace Lab3Using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string pathToInputFile = "../../../input.txt";
+
+            string pathToOutputFile = "../../../output.txt";
+
+
+            ClassLab3.Run(pathToInputFile, pathToOutputFile);
+        }
+    }
+}
